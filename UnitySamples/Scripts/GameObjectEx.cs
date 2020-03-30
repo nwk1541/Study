1c574bd53e3c504d1747c44a8dbd7b738f47a814
nwk1541 <nwk1541@gmail.com>
2020-03-30 13:32:07 +0900

    * 공용 스크립트 추가

---
+++ UnitySamples/Scripts/GameObjectEx.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class GameObjectEx
+{
+    public static GameObject FindObject(this GameObject parent, string name, bool includeInactive = true)
+    {
+        Transform[] tf = parent.GetComponentsInChildren<Transform>(includeInactive);
+        foreach (Transform t in tf)
+        {
+            if (t.name == name)
+            {
+                return t.gameObject;
+            }
+        }
+        return null;
+    }
+}
---
+++ UnitySamples/Scripts/TransformEx.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class TransformEx
+{
+    public static Transform FindChildObject(this Transform target, string name)
+    {
+        Transform obj = target.Find(name);
+
+        if (obj != null)
+            return obj;
+
+        foreach (Transform child in target)
+        {
+            obj = child.Find(name);
+            if (obj != null)
+                return obj;
+        }
+
+        return null;
+    }
+}
