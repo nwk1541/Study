3a996b83c197e76d8b45b4577f8373c7989517a6
nwk1541 <nwk1541@gmail.com>
2020-04-23 16:28:39 +0900

    * 기존에 작성해놓았던 유니티 샘플에서 아무 스크립트들 가져와서 추가

---
+++ ExternalTools/SonarQubeSample/Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyData
+{
+    public class _EnemyData
+    {
+
+    }
+
+    List<_EnemyData> enemies = new List<_EnemyData>();
+
+    public void Open(object datas)
+    {
+
+    }
+}
---
+++ ExternalTools/SonarQubeSample/Assets/Scripts/Dummy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public float speed = 5f;
+    public float directionChangeInterval = 1f;
+    public float maxHeadingChange = 30f;
+    public float elasedTimeMax = 0.1f;
+
+    CharacterController controller;
+    Vector3 targetRotation;
+    float heading;
+    float elapsed;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        heading = Random.Range(0, 360f);
+        transform.eulerAngles = new Vector3(0f, heading, 0f);
+    }
+
+    private void Update()
+    {
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+        Vector3 dir = transform.TransformDirection(Vector3.forward);
+        controller.SimpleMove(dir * speed);
+
+        elapsed += Time.deltaTime;
+        if(elapsed > elasedTimeMax)
+        {
+            NewRoute();
+            elapsed = 0f;
+        }
+    }
+
+    void NewRoute()
+    {
+        float floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+        float ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+        heading = Random.Range(floor, ceil);
+        targetRotation = new Vector3(0f, heading, 0f);
+    }
+}
