62828c7eafb1e54a4ab956cdcd919662692e91ca
nwk1541 <nwk1541@naver.com>
2020-05-07 12:45:36 +0900

    * SpriteAtlas 애셋번들 테스트중

--- UnitySamples/AssetBundle/Assets/Scripts/Bootstrap.cs
+++ UnitySamples/AssetBundle/Assets/Scripts/Bootstrap.cs
@@ -6,6 +6,8 @@ using UnityEngine.U2D;
 
 public class Bootstrap : MonoBehaviour
 {
+    public Transform canvas;
+
     void Start()
     {
 
@@ -41,7 +43,45 @@ public class Bootstrap : MonoBehaviour
 
         if(GUI.Button(new Rect(0, 300, 100, 100), "LoadPanel"))
         {
-            //AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/")
+            AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/samplepanel.unity3d");
+            GameObject go = bundle.LoadAsset("samplepanel") as GameObject;
+            go = Instantiate(go);
+
+            go.transform.SetParent(canvas.transform);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localScale = Vector3.one;
+            RectTransform rect = go.GetComponent<RectTransform>();
+            rect.offsetMin = Vector2.zero;
+            rect.offsetMax = Vector2.zero;
+        }
+
+        if (GUI.Button(new Rect(0, 400, 100, 100), "LoadPanel2"))
+        {
+            AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/samplepanel2.unity3d");
+            GameObject go = bundle.LoadAsset("samplepanel2") as GameObject;
+            go = Instantiate(go);
+
+            go.transform.SetParent(canvas.transform);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localScale = Vector3.one;
+            go.transform.localRotation = Quaternion.identity;
+            RectTransform rect = go.GetComponent<RectTransform>();
+            rect.offsetMin = Vector2.zero;
+            rect.offsetMax = Vector2.zero;
+        }
+
+        if (GUI.Button(new Rect(0, 500, 100, 100), "LoadPanelDependency"))
+        {
+            AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/common.unity3d");
+            SpriteAtlas go = bundle.LoadAsset("common") as SpriteAtlas;
+            //AssetBundle bundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/Windows");
+            //AssetBundleManifest manifest = bundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+            //string[] dependencies = manifest.GetAllDependencies("samplepanel.unity3d");
+            //foreach (string depend in dependencies)
+            //{
+            //    string path = Path.Combine(Application.streamingAssetsPath + "/Windows", depend);
+            //    AssetBundle.LoadFromFile(path);
+            //}
         }
     }
 }
