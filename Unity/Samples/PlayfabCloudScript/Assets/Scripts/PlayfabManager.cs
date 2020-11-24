44451c62a33c36917aa8894fec7b195bb7169ee9
wknam <nwk1541@naver.com>
2020-11-24 10:21:02 +0900

    * 플레이팹 프로젝트 추가 ** 플레이팹 SDK 추가 * 코루틴.md 위치 이동

---
+++ Unity/Samples/PlayfabCloudScript/Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    private const string titleID = "7FB1B";
+
+    public static void OnLoginPlayfab()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            /*
+            Please change the titleId below to your own titleId from PlayFab Game Manager.
+            If you have already set the value in the Editor Extensions, this can be skipped.
+            */
+            PlayFabSettings.staticSettings.TitleId = titleID;
+        }
+
+        LoginWithCustomIDRequest request = new LoginWithCustomIDRequest { CustomId = SystemInfo.deviceUniqueIdentifier, CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, (LoginResult loginResult) =>
+        {
+            Debug.Log("Congratulations, you made your first successful API call!");
+        }, OnPlayfabError);
+    }
+
+    public static void OnHelloWorld()
+    {
+        ExecuteCloudScriptRequest req = new ExecuteCloudScriptRequest();
+        req.FunctionName = "helloWorld";
+        req.GeneratePlayStreamEvent = true;
+
+        PlayFabClientAPI.ExecuteCloudScript(req, (ExecuteCloudScriptResult result) =>
+        {
+            Debug.Log(req.FunctionName + ":" + result.Logs[0].Level + " " + result.Logs[0].Message);
+        }, OnPlayfabError);
+    }
+
+    private static void OnPlayfabError(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
---
+++ Unity/Samples/PlayfabCloudScript/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+#if UNITY_EDITOR
+    GUIStyle guiStyle;
+    bool isHideOnGUI;
+
+    private void Start()
+    {
+        guiStyle = new GUIStyle();
+        guiStyle.fontSize = 28;
+
+        isHideOnGUI = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isHideOnGUI = !isHideOnGUI;
+    }
+
+    private void OnGUI()
+    {
+        if (!isHideOnGUI)
+            return;
+
+        if (GUI.Button(new Rect(0, Screen.height * 0.8f, Screen.width * 0.1f, Screen.height * 0.1f), "HelloWorld"))
+        {
+            PlayfabManager.OnHelloWorld();
+        }
+
+        if (GUI.Button(new Rect(0f, Screen.height * 0.9f, Screen.width * 0.1f, Screen.height * 0.1f), "Login"))
+        {
+            PlayfabManager.OnLoginPlayfab();
+        }
+    }
+#endif
+}
