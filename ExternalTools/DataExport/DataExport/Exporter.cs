3d864484498e769530a580b6b9d85e04a3d642f3
wknam <nwk1541@naver.com>
2021-06-17 17:19:46 +0900

    * DataExport 솔루션 추가

---
+++ ExternalTools/DataExport/DataExport/Exporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExport
+{
+    public static class Exporter
+    {
+        public static IExportFormat exportFormat;
+
+        public static void BeginExport()
+        {
+            
+        }
+    }
+}
