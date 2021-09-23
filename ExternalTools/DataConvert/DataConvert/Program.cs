43bf1daf1456b67e5996f87716c652b2401d81a2
wknam <nwk1541@naver.com>
2021-09-23 14:54:03 +0900

    * 프로젝트 생성 및 클래스 추가

---
+++ ExternalTools/DataConvert/DataConvert/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter();
+            converter.Run();
+        }
+    }
+}
