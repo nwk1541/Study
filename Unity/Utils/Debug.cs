403f02fe180165376d555a993bb972188e939133
nwk1541 <nwk1541@naver.com>
2022-07-05 13:01:29 +0900

    * 디버그 유틸 수정

--- Unity/Utils/Debug.cs
+++ Unity/Utils/Debug.cs
@@ -1,73 +1,68 @@
-#if UNITY_EDITOR || DEVEOPMENT_BUILD
-#define DEBUG
-#endif
-
-using UnityEngine;
 using System.Diagnostics;
 
 public static class Debug
 {
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void Log(object msg)
     {
         UnityEngine.Debug.Log(msg);
     }
 
-    [Conditional("DEBUG")]
-    public static void Log(object msg, Object context)
+    [Conditional("UNITY_EDITOR")]
+    public static void Log(object msg, UnityEngine.Object context)
     {
         UnityEngine.Debug.Log(msg, context);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void LogFormat(string format, params object[] args)
     {
         UnityEngine.Debug.LogFormat(format, args);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void LogError(object msg)
     {
         UnityEngine.Debug.LogError(msg);
     }
 
-    [Conditional("DEBUG")]
-    public static void LogError(object msg, Object context)
+    [Conditional("UNITY_EDITOR")]
+    public static void LogError(object msg, UnityEngine.Object context)
     {
         UnityEngine.Debug.LogError(msg, context);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void LogErrorFormat(string format, params object[] args)
     {
         UnityEngine.Debug.LogErrorFormat(format, args);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void LogWarning(object msg)
     {
         UnityEngine.Debug.LogWarning(msg);
     }
 
-    [Conditional("DEBUG")]
-    public static void LogWarning(object msg, Object context)
+    [Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object msg, UnityEngine.Object context)
     {
         UnityEngine.Debug.LogWarning(msg, context);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void LogWarningFormat(string format, params object[] args)
     {
         UnityEngine.Debug.LogWarningFormat(format, args);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void Assert(bool cond, string format)
     {
         UnityEngine.Debug.Assert(cond, format);
     }
 
-    [Conditional("DEBUG")]
+    [Conditional("UNITY_EDITOR")]
     public static void AssertFormat(bool cond, string format, params object[] args)
     {
         UnityEngine.Debug.AssertFormat(cond, format, args);
